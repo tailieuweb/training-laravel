0459079dfdab4302aaad0db37ca681ae1fc510c9
tailieuweb <tailieuweb.com@gmail.com>
2024-03-07 14:41:43 +0700

    Commit

---
+++ storage/framework/views/40d17b5b9e61543368e4d954e08219041336c3f8.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Custom Auth in Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+    <div class="container">
+        <a class="navbar-brand mr-auto" href="#">PositronX</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <?php if(auth()->guard()->guest()): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo e(route('login')); ?>">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo e(route('register-user')); ?>">Register</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo e(route('signout')); ?>">Logout</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<?php echo $__env->yieldContent('content'); ?>
+</body>
+</html>
+<?php /**PATH D:\1-github-workspace\1-tailieuweb\21-training-laravel\training-laravel\resources\views/dashboard.blade.php ENDPATH**/ ?>
