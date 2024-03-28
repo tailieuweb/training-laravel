2f3ea9669f2a417a576147d21db38ef2e89551db
tailieuweb <tailieuweb.com@gmail.com>
2024-03-28 15:12:05 +0700

    Add CRUD user

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Hash;
 use Session;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class CustomAuthController extends Controller
+class CrudUserController extends Controller
 {
 
     public function index()
