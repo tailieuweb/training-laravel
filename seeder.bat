822acabdb11710ec9750acc03e61346a797d20df
tailieuweb <tailieuweb.com@gmail.com>
2024-04-27 09:27:40 +0700

    Add file bat

---
+++ seeder.bat
@@ -0,0 +1,6 @@
+php artisan migrate
+php artisan db:seed --class=UserSeeder
+php artisan db:seed --class=FavoriteSeeder
+php artisan db:seed --class=PostsSeeder
+php artisan db:seed --class=UserFavoriteSeeder
+php artisan db:seed --class=ProfileSeeder
