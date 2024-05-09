f789a9fc06422465249dd1acdae1991d2eefa0d2
tailieuweb <tailieuweb.com@gmail.com>
2024-05-09 14:46:19 +0700

    Add assets

--- resources/views/crud_user/list.blade.php
+++ resources/views/crud_user/list.blade.php
@@ -1,36 +1,4 @@
 @extends('dashboard')
-<style>
-    .danhSach{
-        text-align: center;
-        margin-bottom: 20px ;
-    }
-   table thead tr th{
-        border: 1px solid #000;
-        text-align: center;
-    }
-    table tbody tr th {
-        border-left: 1px solid #000;
-        border-right: 1px solid #000;
-        font-weight: normal;
-
-    }
-    table {
-        border: 1px solid #000;
-        margin-bottom: 70px ;
-    }
-    table tbody tr th a{
-        text-decoration: none;
-         color: black;
-
-    }
-    .link {
-        text-align: center;
-        position: absolute;
-        /* top: 150%; */
-        left: 45%;
-
-    }
-</style>
 @section('content')
 <h5 class="danhSach">Danh sách user</h5>
     <main class="login-form">
