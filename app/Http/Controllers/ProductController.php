db4f7a2586fcb5b155b50161731b863775da6305
tailieuweb <tailieuweb.com@gmail.com>
2024-05-10 08:56:04 +0700

    API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\BaseController as BaseController;
 use App\Models\Product;
 use Validator;
 use App\Http\Resources\ProductResource;
