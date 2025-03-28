0bbb4db6af1f2767bd47b9843928dedabc322066
tailieuweb <tailieuweb.com@gmail.com>
2025-03-28 10:09:48 +0700

    Fix review

--- resources/views/crud_user/update.blade.php
+++ resources/views/crud_user/update.blade.php
@@ -11,12 +11,22 @@
                             <form action="{{ route('user.postUpdateUser') }}" method="POST">
                                 @csrf
                                 <input name="id" type="hidden" value="{{$user->id}}">
+
                                 <div class="form-group mb-3">
                                     <input type="text" placeholder="Name" id="name" class="form-control" name="name"
                                            value="{{ $user->name }}"
                                            required autofocus>
                                     @if ($errors->has('name'))
                                         <span class="text-danger">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email" class="form-control" name="email"
+                                           value="{{ $user->email }}"
+                                           required autofocus>
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
                                     @endif
                                 </div>
 
