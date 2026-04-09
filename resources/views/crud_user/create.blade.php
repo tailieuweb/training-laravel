fbe675ff17ad758ada2025a4e06dec5024fb4d23
Tailieuweb <tailieuweb.com@gmail.com>
2026-04-09 13:57:33 +0700

    Read guide and implement CRUD user

---
+++ resources/views/crud_user/create.blade.php
@@ -0,0 +1,64 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="signup-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">Create User</h3>
+                        <div class="card-body">
+                            <form action="{{ route('user.postUser') }}" method="POST">
+                                @csrf
+
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                           required autofocus>
+                                    @if ($errors->has('name'))
+                                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Phone" id="name" class="form-control" name="phone"
+                                           required autofocus>
+                                    @if ($errors->has('phone'))
+                                        <span class="text-danger">{{ $errors->first('phone') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Address" id="name" class="form-control" name="address"
+                                           required autofocus>
+                                    @if ($errors->has('address'))
+                                        <span class="text-danger">{{ $errors->first('address') }}</span>
+                                    @endif
+                                </div>
+
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                           name="email" required autofocus>
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="Password" id="password" class="form-control"
+                                           name="password" required>
+                                    @if ($errors->has('password'))
+                                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                                <div class="d-grid mx-auto">
+                                    <button type="submit" class="btn btn-dark btn-block">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/crud_user/update.blade.php
@@ -0,0 +1,60 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="signup-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">Update User</h3>
+                        <div class="card-body">
+                            <form action="{{ route('user.postUpdateUser') }}" method="POST">
+                                @csrf
+                                <input name="id" type="hidden" value="{{$user->id}}">
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                           value="{{ $user->name }}"
+                                           required autofocus>
+                                    @if ($errors->has('name'))
+                                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="phone" class="form-control" name="phone"
+                                           value="{{ $user->phone }}"
+                                           required autofocus>
+                                    @if ($errors->has('phone'))
+                                        <span class="text-danger">{{ $errors->first('phone') }}</span>
+                                    @endif
+                                </div>
+
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                           value="{{ $user->email }}"
+                                           name="email" required autofocus>
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="Password" id="password" class="form-control"
+                                           name="password" required>
+                                    @if ($errors->has('password'))
+                                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="d-grid mx-auto">
+                                    <button type="submit" class="btn btn-dark btn-block">Update</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
